24fa1b15bbce5dac956d43ddef88884ba81d720d
Harsh Sharma <89311303+SammyHarsh@users.noreply.github.com>
2022-11-03 12:29:28 -0700

    Add files via upload
    New Master

--- Master.java
+++ Master.java
@@ -16,12 +16,11 @@ public class Master extends JDialog
     private JButton saveButton;
     private JButton cancelButton;
     private JPanel MasterJ;
-
+    private JButton searchButton;
 
 
     public Master() {
-
-
+        final String[] flag = {""};
         cancelButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -33,10 +32,103 @@ public class Master extends JDialog
             @Override
             public void actionPerformed(ActionEvent e) {
 
-              save();
+            if (flag[0].equals("Update"))
+            {
+               // JOptionPane.showMessageDialog(null,"Update");
+                update();
+            }
+            else
+            {
+                save();
+            }
+
+            }
+        });
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    fetch();
+                    flag[0] ="Update";
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+
+
             }
         });
     }
+
+    private void update()
+    {
+        String MasterCode = tfmastercode.getText();
+        String MasterType = tfmastertype.getText();
+        String MasterDesc = tfmasterdescription.getText();
+
+        final String DB_URL = "jdbc:mysql://localhost:3306/sms";
+        final String USERNAME = "root";
+        final String PASSWORD = "123456";
+
+        try {
+
+            Connection conn = DriverManager.getConnection(DB_URL,USERNAME,PASSWORD);
+            //  JOptionPane.showMessageDialog(null,"Near Connection");
+            Statement stmt;
+            stmt = conn.createStatement();
+            String  sql = "Update master " + "set master_type ='" + tfmastertype.getText().toString()+ "',master_desc = '" +tfmasterdescription.getText().toString()+
+                    "' where master_code = '"+tfmastercode.getText().toString()+"'";
+
+            PreparedStatement prepareStatement = conn.prepareStatement(sql);
+
+
+            //  JOptionPane.showMessageDialog(this,prepareStatement);
+            int addedRows = (prepareStatement).executeUpdate();
+            JOptionPane.showMessageDialog(this,"Record Updated Sucessfully");
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            //  JOptionPane.showMessageDialog(null,"Error");
+        }
+    }
+
+    private void fetch() throws SQLException {
+
+      //  if (MasterCode.isEmpty())
+        {
+         //   JOptionPane.showMessageDialog( this,
+           //         "Please enter all fields, Can not be empty",
+             //       "Try again",
+               //     JOptionPane.ERROR_MESSAGE);
+          //  return;
+
+        }
+
+        final String DB_URL = "jdbc:mysql://localhost:3306/sms";
+        final String USERNAME = "root";
+        final String PASSWORD = "123456";
+        try {
+
+            Connection conn1 = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            String Data = tfmastercode.getText().toString();
+            String sql = "select * FROM sms.master where master_code = '"+tfmastercode.getText().toString()+"';";
+            ResultSet rs = conn1.createStatement().executeQuery(sql);
+
+            while (rs.next()){
+                tfmastercode.setText(rs.getString("master_code").toString());
+                tfmastertype.setText(rs.getString("master_type").toString());
+                tfmasterdescription.setText(rs.getString("master_desc").toString());
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        JOptionPane.showMessageDialog(null,e.getMessage().toString());
+        }
+
+    }
+
+
+
 
     private void save() {
 
@@ -98,8 +190,6 @@ public class Master extends JDialog
             prepareStatement.setString( 3,masterdescription);
 
 
-            ;
-
             //    JOptionPane.showMessageDialog(this,prepareStatement);
 
             //  JOptionPane.showMessageDialog(this,prepareStatement);
@@ -132,6 +222,7 @@ public class Master extends JDialog
 
     frame.setVisible(true);
     frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
     }
 
 }
